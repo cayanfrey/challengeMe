9df47a1cf81b3b541b66a7b5121dea0f9aa4a5de
Cayan <cayan.frey@hotmail.com>
2013-03-18 08:44:27 +0100

    Add project

---
+++ build/generated-sources/ap-source-output/ch/comem/models/Groupe_.java
@@ -0,0 +1,14 @@
+package ch.comem.models;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.3.0.v20110604-r9504", date="2013-03-15T16:25:45")
+@StaticMetamodel(Groupe.class)
+public class Groupe_ { 
+
+    public static volatile SingularAttribute<Groupe, Long> id;
+    public static volatile SingularAttribute<Groupe, String> nom;
+
+}
---
+++ build/generated-sources/ap-source-output/ch/comem/models/Membre_.java
@@ -0,0 +1,17 @@
+package ch.comem.models;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.3.0.v20110604-r9504", date="2013-03-15T16:25:45")
+@StaticMetamodel(Membre.class)
+public class Membre_ { 
+
+    public static volatile SingularAttribute<Membre, Long> id;
+    public static volatile SingularAttribute<Membre, String> prenom;
+    public static volatile SingularAttribute<Membre, Integer> point;
+    public static volatile SingularAttribute<Membre, String> email;
+    public static volatile SingularAttribute<Membre, String> nom;
+
+}
